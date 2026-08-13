e2e7bf7e8d4d786ab61b564a0349a084fa102e1e
Akmalov Dilmurod <dilmurodakmalov20@gmail.com>
2026-08-13 15:04:02 +0500

    dto for posts

---
+++ apps/api/src/posts/dto/create-post.dto.ts
@@ -0,0 +1,24 @@
+import { IsIn, IsOptional, IsString, IsUrl, MaxLength, MinLength, ValidateIf } from 'class-validator';
+
+export class CreatePostDto {
+  @IsIn(['TEXT', 'LINK'])
+  type!: 'TEXT' | 'LINK';
+
+  @IsString()
+  @MinLength(1)
+  @MaxLength(300)
+  title!: string;
+
+  @ValidateIf((o) => o.type === 'TEXT')
+  @IsString()
+  @MaxLength(40000)
+  body?: string;
+
+  @ValidateIf((o) => o.type === 'LINK')
+  @IsUrl({ protocols: ['http', 'https'], require_protocol: true })
+  url?: string;
+
+  @IsOptional()
+  @IsString()
+  subreddit!: string;
+}
